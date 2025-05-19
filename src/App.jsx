bd52ff195acbadff27a9c769e0b55ec3831cfd67
Emil <somaleyoo123@gmail.com>
2025-05-19 09:40:18 +0200

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import { useState } from 'react'
-
+import jojobg from '/src/assets/images/jojos-bizarre-adventure.jpg'
 import './App.css'
+import Stock from './components/Stock/stock'
 
 function App() {
 
 
   return (
     <>
-      <div className="header"></div>
-      <h1>JOJO NO KIMYOU NA BOUKEN</h1>
+
+ 
+      <div className="header">
+        <h1>JOJO NO KIMYOU NA BOUKEN</h1>
+        <img className='bg-header' src={jojobg} alt="" />
+      </div>
+      <div className="card-container">
+        <Stock/>
+      </div>
     </>
   )
 }
--- src/components/Stock/stock.jsx
+++ src/components/Stock/stock.jsx
@@ -3,7 +3,9 @@ import './stock.css'
 export default function Stock(props) {
     return(
         <>
-            
+            <div className="card">
+
+            </div>
         </>
     )
 }   
